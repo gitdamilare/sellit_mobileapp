06d10a8530b96ed24f2368215d25e9b91f782a61
Oyebanji Oluwadamilare <ocoyebanji@irissmart.com>
2020-01-03 14:20:57 +0100

    Repository Implementation
    Product and User Repository

--- lib/data/userrepository.dart
+++ lib/data/userrepository.dart
@@ -1,36 +1,70 @@
+import 'dart:convert' as convert;
+import 'dart:io';
 
-import 'package:sellit_mobileapp/models/auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:sellit_mobileapp/models/inputDtos/auth.dart';
+import 'package:sellit_mobileapp/models/outputDtos/authoutputDto.dart';
+import 'package:sellit_mobileapp/services/jsonrepo.dart';
+import 'package:sellit_mobileapp/services/root.dart';
+import 'package:sellit_mobileapp/utilis/urlLinks.dart';
+import "package:http/http.dart" as http;
 
-abstract class UserRepository{
-  Future<String> authenticate(Auth input);
+abstract class UserRepository {
+  Future<AuthOutputDto> authenticate(Auth input);
   Future<void> deleteToken();
-  Future<void> persistToken(String token);
+  Future<void> persistToken(AuthOutputDto token);
   Future<bool> hasToken();
 }
 
-class UserServices extends UserRepository{
+class UserServices extends UserRepository {
+final storage = new FlutterSecureStorage();
+
   @override
-  Future<String> authenticate(Auth input) {
-    // TODO: implement authenticate
+  Future<AuthOutputDto> authenticate(Auth input) async {
+    AuthOutputDto result = AuthOutputDto();
+    String url = AuthenicationApi;
+    var jsoninput = convert.jsonEncode(input);
+    debugPrint(jsoninput);
+    try {
+      var response = await http.post(url,
+          body: jsoninput,
+          headers: {HttpHeaders.contentTypeHeader: "application/json"});
+     if(response.statusCode == 200){
+        var jsonBody = response.body;
+        var output = convert.jsonDecode(jsonBody);
+        result = AuthOutputDto.fromJson(output);
+        
+     }
+    } catch (e) {
+      debugPrint(e);
+    }
+    return result;
+  }
+
+  @override
+  Future<void> deleteToken() async {
+    await storage.delete(key: "token");
     return null;
   }
 
   @override
-  Future<void> deleteToken() {
-    // TODO: implement deleteToken
-    return null;
+  Future<bool> hasToken() async {
+   // AuthOutputDto authCheck =
+      //  await JsonRepo.jsonRepoObject.getDatafromJsonFile();
+    String value = await storage.read(key: "token");
+    if (value != null) {
+      return true;
+    }
+    return false;
   }
 
   @override
-  Future<bool> hasToken() {
-    // TODO: implement hasToken
-    return null;
+  Future<void> persistToken(AuthOutputDto input) async {
+    await storage.write(key: "token", value: input.token);
+    Root.rootObject.rootPath = await Root.rootObject.getRootPath();
+    Root.rootObject.localPath = await Root.rootObject.localFile();
+    Root.rootObject.localPath =
+        await JsonRepo.jsonRepoObject.writeAccountDataToJson(input);
   }
-
-  @override
-  Future<void> persistToken(String token) {
-    // TODO: implement persistToken
-    return null;
-  }
-
 }
